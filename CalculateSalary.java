2b447e654912297f0d412691d51545e02f33786f
prabodini-pamuditha <103652087+prabodini-pamuditha@users.noreply.github.com>
2023-01-27 11:57:59 +0530

    Implement calculations

--- CalculateSalary.java
+++ CalculateSalary.java
@@ -1,41 +1,96 @@
 import java.io.*;
 
-class Thread1 extends Thread {
-	
-    public void run() {
-        // executed Thread1
-        System.out.println("Thread is running");
-    }
-}
-
-class Thread2 extends Thread {
-    public void run() {
-        // executed Thread2
-        System.out.println("Thread2 is running");
-    }
-}
-
 public class CalculateSalary {
-    public static void main(String[] args) throws IOException {
-				
+	private static double dayPayment;
+	private static int numOfDays;
+	private static double basicSalary;
+	
+    public static void main(String[] args){
+			
+		/*private double dayPayment;
+		double basicSalary = 0;
+		double finalSalary = 0;
+		double allowances = 0;			
+		private int numOfDays;*/			
+		
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Enter the Day Payment value: ");
-		double dayPayment = Double.parseDouble(input.readLine());
-		System.out.println("Entered Day Payment value is: " + dayPayment);
 		
-		System.out.println("Enter the Number of Days employee works: ");
-		int numOfDays = Integer.parseInt(input.readLine());
-		System.out.println("Entered Days are: " + numOfDays);
-        
+		//get dayPayment & numOfDays from user
+		try{
+			System.out.println("Enter the Day Payment value: ");
+			double dayPayment = Double.parseDouble(input.readLine());
+			System.out.println("Enter the Number of Days: ");
+			int numOfDays = Integer.parseInt(input.readLine());
+			
+			// calculate basicSalary
+			double basicSalary = dayPayment * numOfDays;
+			System.out.println("Basic Salary is: " + basicSalary);
+		}catch(IOException e){
+			System.out.println("Not entered the values");
+		}
+		        
 		// create two new threads
-        Thread thread1 = new Thread1();
-        Thread thread2 = new Thread2();
+        Thread thread2 = new Thread(new AllowancesThread2(basicSalary));
+        Thread thread3 = new Thread(new EPFThread3(basicSalary));
 		
-        // start the threads
-        thread1.start();
+        // schedule threads
         thread2.start();
+        thread3.start();
 		
-        // main thread(CalculateSalary)
-        System.out.println("Main thread is running");
+		try{
+			thread2.join();
+			thread3.join();
+		}catch(InterruptedException e){
+			System.out.println("Thread2, Thread3 are failed to resume");
+		}
+		
+		double allowances = thread2.getAllowances();
+		System.out.println("Allowances: " + allowances);
+		
+		double epf = thread3.getEPF();
+		System.out.println("EPF: " + epf);
+		
+		double finalSalary = basicSalary + allowances + epf;
+		System.out.println("Final Salary is: " + finalSalary);
     }
 }
+
+class AllowancesThread2 implements Runnable {
+	private double basicSalary;
+	private double allowances;
+	
+	public AllowancesThread2(double basicSalary){
+		this.basicSalary = basicSalary;
+	}
+	
+	@Override
+    public void run() {
+		// calculate allowances
+		double allowances = basicSalary * 0.05;
+    }
+	
+	public double getAllowances(){
+		return allowances;
+	}
+}
+
+class EPFThread3 implements Runnable {
+	private double basicSalary;
+	private double epf;
+	
+	public EPFThread3(double basicSalary){
+		this.basicSalary = basicSalary;
+		
+	}
+	
+	@Override
+	public void run() {
+		double epf = basicSalary * 0.08;
+	}
+	
+	public double getEPF(){
+		return epf;
+	}
+}
+
+
